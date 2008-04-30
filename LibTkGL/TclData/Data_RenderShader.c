31c9d2490481eb0f2effd1662cd0ff76f8df64ad
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2008-04-30 18:53:23 +0000

    Fixed RPN integer field not rendered properly, actually, not render at all

--- LibTkGL/TclData/Data_RenderShader.c
+++ LibTkGL/TclData/Data_RenderShader.c
@@ -43,7 +43,7 @@ static int GL_Type[]={ GL_BITMAP,GL_BITMAP,GL_UNSIGNED_BYTE,GL_BYTE,GL_UNSIGNED_
  * Nom      : <Data_RenderShaderParticle>
  * Creation : Octobre 1999 - J.P. Gauthier - CMC/CMOE
  *
- * But      : Effectue les calculs et l'affichage pour le type particule.
+ * But      : Effectue les calculs et l'affichage pour le type particule sur le GPU.
  *
  * Parametres :
  *  <Champs>  : Champs
@@ -91,6 +91,7 @@ int Data_RenderShaderParticle(TData *Field,ViewportItem *VP,Projection *Proj) {
    glBindTexture(GL_TEXTURE_1D,tx[0]);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
    glTexImage1D(GL_TEXTURE_1D,0,GL_RGBA,Field->Spec->Map->NbPixels,0,GL_RGBA,GL_UNSIGNED_BYTE,Field->Spec->Map->Color);
 
    /*Setup 1D Interval Texture*/
@@ -99,7 +100,7 @@ int Data_RenderShaderParticle(TData *Field,ViewportItem *VP,Projection *Proj) {
    if (Field->Spec->InterNb) {
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_RED,GL_FLOAT,Field->Spec->Inter);
+      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_LUMINANCE,GL_FLOAT,Field->Spec->Inter);
    }
 
    /*Setup point sprites*/
@@ -153,6 +154,23 @@ int Data_RenderShaderParticle(TData *Field,ViewportItem *VP,Projection *Proj) {
    return(1);
 }
 
+/*----------------------------------------------------------------------------
+ * Nom      : <Data_RenderShaderMesh>
+ * Creation : Octobre 1999 - J.P. Gauthier - CMC/CMOE
+ *
+ * But      : Effectue les calculs et l'affichage pour le type triangle mesh sur le GPU.
+ *
+ * Parametres :
+ *  <Champs>  : Champs
+ *  <VP>      : Parametres du viewport
+ *  <Proj>    : Parametres de la projection
+ *
+ * Retour:
+ *
+ * Remarques :
+ *
+ *----------------------------------------------------------------------------
+*/
 int Data_RenderShaderMesh(TData *Field,ViewportItem *VP,Projection *Proj) {
 
    int     n;
@@ -186,6 +204,7 @@ int Data_RenderShaderMesh(TData *Field,ViewportItem *VP,Projection *Proj) {
    glBindTexture(GL_TEXTURE_1D,tx[0]);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
    glTexImage1D(GL_TEXTURE_1D,0,GL_RGBA,Field->Spec->Map->NbPixels,0,GL_RGBA,GL_UNSIGNED_BYTE,Field->Spec->Map->Color);
 
    /*Setup 1D Interval Texture*/
@@ -194,7 +213,7 @@ int Data_RenderShaderMesh(TData *Field,ViewportItem *VP,Projection *Proj) {
    if (Field->Spec->InterNb) {
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_RED,GL_FLOAT,Field->Spec->Inter);
+      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_LUMINANCE,GL_FLOAT,Field->Spec->Inter);
    }
    glUniform1iARB(GLShader_UniformGet(prog,"Colormap"),0);
    glUniform1iARB(GLShader_UniformGet(prog,"Interval"),1);
@@ -221,11 +240,133 @@ int Data_RenderShaderMesh(TData *Field,ViewportItem *VP,Projection *Proj) {
 }
 
 /*----------------------------------------------------------------------------
+ * Nom      : <Data_RenderShaderStream>
+ * Creation : Avril 2008 - J.P. Gauthier - CMC/CMOE
+ *
+ * But      : Effectue l'affichage de streamlines de champs vectoriels sur le GPU.
+ *
+ * Parametres :
+ *  <Field>   : Champs
+ *  <VP>      : Parametres du viewport
+ *  <Proj>    : Parametres de la projection
+ *
+ * Retour:
+ *
+ * Remarques :
+ *
+ *----------------------------------------------------------------------------
+*/
+int Data_RenderShaderStream(TData *Field,ViewportItem *VP,Projection *Proj){
+
+   double i,j,width,dt;
+   int    b,f,len,pi,pj,dz;
+   float  step;
+   Vect3d pix;
+   Coord  coo;
+
+   extern Vect3d GDB_VBuf[];
+
+   if (GLRender->Resolution>2) {
+      return(0);
+   }
+
+   if (!Field->Ref || !Field->Ref->Pos || !Field->Spec->Outline) {
+      return(0);
+   }
+
+   /*Setup 1D Texture*/
+   glTexImage1D(GL_TEXTURE_1D,0,GL_RGBA,256,0,GL_RGBA,GL_UNSIGNED_BYTE,StreamMap);
+   glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+   glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_DECAL);
+   glEnable(GL_TEXTURE_1D);
+
+   if (!Field->Map) {
+      Field->Map=(float*)malloc(sizeof(float)*STREAMLEN);
+      for(len=0;len<STREAMLEN;len++) {
+         Field->Map[len]=(float)len/STREAMLEN*32;
+      }
+
+   }
+
+   glMatrixMode(GL_TEXTURE);
+   if (GLRender->Delay<2000) {
+      Field->Spec->TexStep+=0.01;
+      Field->Spec->TexStep=Field->Spec->TexStep>1.0?0.0:Field->Spec->TexStep;
+  }
+
+   glEnable(GL_STENCIL_TEST);
+   glStencilMask(0x2);
+   glStencilFunc(GL_NOTEQUAL,0x2,0x2);
+   glStencilOp(GL_KEEP,GL_KEEP,GL_REPLACE);
+
+   glReadBuffer(GL_STENCIL);
+   glEnableClientState(GL_VERTEX_ARRAY);
+   glColor3us(Field->Spec->Outline->red,Field->Spec->Outline->green,Field->Spec->Outline->blue);
+   glEnable(GL_BLEND);
+
+   pi=pj=-1;
+   len=512;
+   dz=Field->Spec->Sample*10;
+   dt=0.0;
+
+   width=Field->Spec->RenderTexture?Field->Spec->Size*0.2:Field->Spec->Size*0.1;
+
+   /*Recuperer les latlon des pixels sujets*/
+   for (pix[0]=0;pix[0]<VP->Width;pix[0]+=dz) {
+      for (pix[1]=0;pix[1]<VP->Height;pix[1]+=dz) {
+
+         Proj->Type->UnProject(VP,Proj->Params,&coo,pix);
+         if (coo.lat==-999.0) {
+            continue;
+         }
+
+         if (Field->Ref->UnProject(Field->Ref,&i,&j,coo.lat,coo.lon,0,1) && i<Field->Def->NI-2 && j<Field->Def->NJ-2) {
+            /*Get the cell resolution, if not the same, to use as step size for a constant spacing*/
+            if (pi!=(int)i ||  pj!=(int)j) {
+               pi=i;
+               pj=j;
+               step=5.0/FFCellResolution(VP,Proj,Field->Ref->Pos[FIDX2D(Field->Def,pi,pj)],Field->Ref->Pos[FIDX2D(Field->Def,pi+1,pj+1)]);
+            }
+
+
+            /*Get the streamline */
+            b=FFStreamLine(Field->Ref,Field->Def,VP,GDB_VBuf,NULL,i,j,Field->Def->Level,len,-step,Field->Spec->Min,0,REF_PROJ,0);
+            f=FFStreamLine(Field->Ref,Field->Def,VP,&GDB_VBuf[len],NULL,i,j,Field->Def->Level,len,step,Field->Spec->Min,0,REF_PROJ,0);
+
+            /* If we have at least some part of it */
+            glPushMatrix();
+            glTranslatef(-Field->Spec->TexStep-(dt+=0.15),0.0,0.0);
+            if (b+f>2) {
+               glLineWidth(width);
+               glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE);
+               Proj->Type->Render(Proj,0,&GDB_VBuf[len-b],NULL,NULL,Field->Map,GL_LINE_STRIP,b+f,NULL,NULL);
+
+               glLineWidth(8*width);
+               glColorMask(GL_FALSE,GL_FALSE,GL_FALSE,GL_FALSE);
+               Proj->Type->Render(Proj,0,&GDB_VBuf[len-b],NULL,NULL,NULL,GL_LINE_STRIP,b+f,NULL,NULL);
+            }
+            glPopMatrix();
+         }
+      }
+   }
+
+   glClear(GL_STENCIL_BUFFER_BIT);
+   glStencilMask(0xf);
+   glStencilFunc(GL_EQUAL,0x0,0xf);
+   glStencilOp(GL_KEEP,GL_KEEP,GL_KEEP);
+   glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE);
+   glDisable(GL_TEXTURE_1D);
+   glDisableClientState(GL_VERTEX_ARRAY);
+
+   return(1);
+}
+/*----------------------------------------------------------------------------
  * Nom      : <Data_RenderShaderTexture>
  * Creation : Septembre 2000 - J.P. Gauthier - CMC/CMOE
  *
- * But      : Effectue l'affichage de la texture du champs en
- *            Adaptive Meshing.
+ * But      : Effectue l'affichage de la texture du champs sur le GPU.
  *
  * Parametres  :
  *  <Field>    : Champs
@@ -243,6 +384,7 @@ int Data_RenderShaderTexture(TData *Field,ViewportItem *VP,Projection *Proj){
    int     dr,ri,rj,i,j,idxk,idx0,idx1,ox;
    float   min,rng;
    Vect3d *pos;
+   float  *buf;
    char   *ptr;
 
    GLuint      tx[3];
@@ -290,7 +432,7 @@ int Data_RenderShaderTexture(TData *Field,ViewportItem *VP,Projection *Proj){
    glBindTexture(GL_TEXTURE_1D,tx[0]);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
    glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+   glTexParameteri(GL_TEXTURE_1D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
    glTexImage1D(GL_TEXTURE_1D,0,GL_RGBA,Field->Spec->Map->NbPixels,0,GL_RGBA,GL_UNSIGNED_BYTE,Field->Spec->Map->Color);
 
    /*Setup 1D Interval Texture*/
@@ -299,7 +441,7 @@ int Data_RenderShaderTexture(TData *Field,ViewportItem *VP,Projection *Proj){
    if (Field->Spec->InterNb) {
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
       glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_RED,GL_FLOAT,Field->Spec->Inter);
+      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Spec->InterNb,1,0,GL_LUMINANCE,GL_FLOAT,Field->Spec->Inter);
    }
 
    /*Setup 2D Data Texture*/
@@ -307,8 +449,19 @@ int Data_RenderShaderTexture(TData *Field,ViewportItem *VP,Projection *Proj){
    glBindTexture(GL_TEXTURE_RECTANGLE_ARB,tx[2]);
    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-//   glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Def->NI,Field->Def->NJ,0,GL_RED,GL_FLOAT,ptr);
-   glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Def->NI,Field->Def->NJ,0,GL_RED,GL_Type[Field->Def->Type],ptr);
+
+   /*Why the hell doesn't GL_FLOAT_R32_NV accepts only Float32, I don't know, here's the quick fix*/
+   if (Field->Def->Type!=TD_Float32) {
+      if (buf=(float*)malloc(Field->Def->NI*Field->Def->NJ*sizeof(float))) {
+         for(i=0;i<Field->Def->NI*Field->Def->NJ;i++) {
+             Def_GetMod(Field->Def,idxk+i,buf[i]);
+         }
+         glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Def->NI,Field->Def->NJ,0,GL_LUMINANCE,GL_FLOAT,buf);
+         free(buf);
+      }
+   } else {
+      glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,0,GL_FLOAT_R32_NV,Field->Def->NI,Field->Def->NJ,0,GL_LUMINANCE,GL_Type[Field->Def->Type],ptr);
+   }
 
    glUniform1iARB(GLShader_UniformGet(prog,"Colormap"),0);
    glUniform1iARB(GLShader_UniformGet(prog,"Interval"),1);
