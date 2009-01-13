a9615e469d5abf985f94458257032fe92facdc25
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2009-01-13 15:33:15 +0000

    Removed the shared objects from svn.

--- eer_SPI/Shared/IRIX64/TclTk/lib/tcllib1.8/sasl/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-# pkgIndex.tcl                                                -*- tcl -*-
-# Copyright (C) 2005 Pat Thoyts <patthoyts@users.sourceforge.net>
-# $Id: pkgIndex.tcl,v 1.3 2005/09/19 12:12:39 patthoyts Exp $
-if {![package vsatisfies [package provide Tcl] 8.2]} {
-    # PRAGMA: returnok
-    return
-}
-package ifneeded SASL 1.0.0 [list source [file join $dir sasl.tcl]]
-package ifneeded SASL::NTLM 1.0.0 [list source [file join $dir ntlm.tcl]]
--- eer_SPI/Shared/Linux/TclTk/lib/Tclexpat3.1/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded xml::expat 3.1  [list load [file join $dir libTclexpat3.1.so]]
