eb5d5b28f70cf0cbdc1d52ceb6abd0d551455350
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2009-08-24 18:49:02 +0000

    Fixed invalid coloring of data when using RSMC intervals and contours of empty field.

--- LibTkGL/TclData/Data_Render.c
+++ LibTkGL/TclData/Data_Render.c
@@ -237,7 +237,7 @@ int Data_Render(Tcl_Interp *Interp,TData *Field,ViewportItem *VP,ClientData Proj
       /*Verifier la presence d'une palette de couleur si elle est necessaire*/
       if (Field->Spec->Map) {
 
-         if (Field->Spec->RenderTexture && (!Field->Spec->RenderVol || Field->Ref->Grid[0]=='V')) {
+         if (Field->Spec->RenderTexture && (!Field->Spec->RenderVol || Field->Ref->Grid[0]=='V') && (!Field->Spec->RenderContour || Field->Spec->InterNb)) {
             if (Field->Ref->Grid[0]!='X' && Field->Ref->Grid[0]!='Y' && Field->Ref->Grid[1]!='Y') {
                if (GLRender->ShaderAvailable) {
                   nras+=Data_RenderShaderTexture(Field,VP,(Projection*)Proj);
