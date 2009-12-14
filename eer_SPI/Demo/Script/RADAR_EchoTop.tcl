041a67e8378d7c390d5ce625c7af7b6e0368232a
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2009-12-14 15:13:26 +0000

    Output ASCII file of radar echo top.

--- eer_SPI/Demo/Script/RADAR_EchoTop.tcl
+++ eer_SPI/Demo/Script/RADAR_EchoTop.tcl
@@ -20,6 +20,10 @@ exec $SPI_PATH/tclsh "$0" "$@"
 #
 #============================================================================
 
+package require TclData
+
+puts \n[file tail [info script]]
+
 proc GridDefineLL { Lat0 Lon0 Lat1 Lon1 DLat DLon { ETIKET GRID }} {
 
    set ni [expr int(ceil(($Lon1-$Lon0)/$DLon))]
@@ -57,60 +61,70 @@ proc GridDefineLL { Lat0 Lon0 Lat1 Lon1 DLat DLon { ETIKET GRID }} {
    return GRIDLLMEM
 }
 
-package require TclData
-
-puts \n[file tail [info script]]
-
-#----- Open the radar file
-
-set scans [radarfile open RADARSITE read DataIn/200609032150..CONVOL.URP.WUJ.RADAR.IRIS]
+#----- Site parameters
 set lat  49.0161
 set lon  -122.4877
 set elev 230.0
 set dt   3.0
 
-#----- Read the first scan
-radarscan read SCAN1 RADARSITE 0
-
-#----- Set radar location since sometimes (most of it) it is not in the file itself
-radarscan define SCAN1 -LOCATION $lat $lon $elev
-
-#----- Copy first sweep and clear to 0
-vexpr TOP SCAN1()()(0)
-radarscan stats TOP -nodata 0.0
-radarscan clear TOP
-
-#----- Get top
-set k 0
-set min [radarscan define SCAN1 -NOISE]
-set min 7
-foreach sweep [radarscan define SCAN1 -SWEEPANGLE] {
-   puts "   Checking sweep level $sweep"
-   vexpr TOP ifelse(SCAN1()()($k)>$min,$k,TOP)
-   incr k
-}
-
-puts "   Calculating echo top"
-for { set i 0 } { $i<[radarscan define TOP -NBAZIMUTH] } { incr i } {
-   puts "   Azimuth $i"
-   for { set j 0 } { $j<[radarscan define TOP -NBBIN] } { incr j } {
-      set k [radarscan stats TOP -gridvalue $i $j]
-      set h [radarscan stats SCAN1 -height $i $j $k]
-      if { $k>0 } {
-         radarscan stats TOP -gridvalue $i $j $h
-      }
-   }
-}
-
-puts "   Interpolating on RPN field"
+#----- Initialize RPN grid
 GridDefineLL [expr $lat-$dt] [expr $lon-$dt] [expr $lat+$dt] [expr $lon+$dt] 0.01 0.01
-radarscan stats TOP -level 1
-
-fstdfield gridinterp GRIDLLMEM TOP
-fstdfield define GRIDLLMEM -IP1 12000
-
 fstdfile open FILE write DataOut/RADAR_EchoTop.fstd
 fstdfield write GRIDLLTIC FILE -32 True
 fstdfield write GRIDLLTAC FILE -32 True
-fstdfield write GRIDLLMEM FILE -32 True
+
+#----- Loop on the radar files
+foreach file [glob DataIn/*IRIS*] {
+   puts "   Processing file $file"
+   set scans [radarfile open RADARSITE read $file]
+   set out   [open [file tail $file] w]
+
+   #----- Read the first volume scan
+   radarscan read SCAN1 RADARSITE 0
+
+   #----- Set radar location since sometimes (most of it) it is not in the file itself
+   radarscan define SCAN1 -LOCATION $lat $lon $elev
+   set date [radarscan define SCAN1 -DATE]
+
+   #----- Copy first sweep and clear to 0
+   vexpr TOP SCAN1()()(0)
+   radarscan stats TOP -nodata 0.0
+   radarscan clear TOP
+
+   #----- Get top sweep with 7Dbz minimum echo
+   set k 0
+   set echo 7
+   foreach sweep [radarscan define SCAN1 -SWEEPANGLE] {
+      puts "      Checking sweep level $sweep"
+      vexpr TOP ifelse(SCAN1()()($k)>$echo,$k,TOP)
+      incr k
+   }
+
+   #----- Process top to get height in meter
+   puts "      Calculating echo top"
+   puts $out [format "%10s %10s %10s %10s %10s %10s" Azimuth Bin Sweep Lat Lon Top(m)]
+   for { set i 0 } { $i<[radarscan define TOP -NBAZIMUTH] } { incr i } {
+      for { set j 0 } { $j<[radarscan define TOP -NBBIN] } { incr j } {
+         set k [radarscan stats TOP -gridvalue $i $j]
+         if { $k>0 } {
+            radarscan stats SCAN1 -level [expr int($k)]
+            set h [radarscan stats SCAN1 -height $i $j $k]
+            set ll [radarscan stats SCAN1 -gridpoint $i $j]
+            radarscan stats TOP -gridvalue $i $j $h
+            puts $out [format "%10i %10i %10i %10.6f %10.6f %10.2f" $i $j [expr int($k)] [lindex $ll 0] [lindex $ll 1] $h]
+         }
+      }
+   }
+
+   radarfile close RADARSITE
+   close $out
+
+   puts "      Interpolating on RPN field"
+   radarscan stats TOP -level 0
+
+   fstdfield gridinterp GRIDLLMEM TOP
+   fstdfield define GRIDLLMEM -IP1 12000 -DATEO [fstdstamp fromseconds $date] -NOMVAR TOP
+   fstdfield write GRIDLLMEM FILE -32 False
+}
+
 fstdfile close FILE
