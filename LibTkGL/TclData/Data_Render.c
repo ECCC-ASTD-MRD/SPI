3490bb010937596616d1ca9d5ec72d8cd58eba26
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2010-06-07 13:52:41 +0000

    Fixed mem fault on contouring whehn grid has an odd/even x/y pairing.

--- LibTkGL/TclData/Data_Render.c
+++ LibTkGL/TclData/Data_Render.c
@@ -106,6 +106,8 @@ int Data_GetContour(int Mode,TData *Field,Projection *Proj,int NbInter,float *In
          if (i1<i0 && j1<j0) {
             break;
          }
+         i1=i1<i0?i0:i1;
+         j1=j1<j0?j0:j1;
 
          /*If we this gridpoint has'nt yet been visited*/
          if (!buf[Field->Def->NI*j+i]) {
