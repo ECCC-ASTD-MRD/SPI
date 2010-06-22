7cfdc35c6ebe19140f2d8fc49b59629b465c17f1
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2010-06-22 13:11:01 +0000

    Added Cyclope binding to KSH logger version.

--- eer_SPI/Script/Logger.sh
+++ eer_SPI/Script/Logger.sh
@@ -13,12 +13,13 @@
 #
 # Global Settings
 #   LOG_LEVEL      Logging level (MUST,ERROR,WARNING,INFO,DEBUG)
-#   LOG_MAIL       EMail address for mail reports
-#   LOG_MAILTITLE Title to include in mail reports
-#   LOG_JOBID      Job Identificator
+#   LOG_MAILTO     EMail address for mail reports
+#   LOG_MAILTITLE  Title to include in mail reports
 #   LOG_FILE       File to use for log (stdout if undefined)
 #   LOG_TIME       Include dates in log
-#   LOG_MODE       Mode de log (AUTO=automated jobs, send emails on error)
+#   LOG_JOBID      Job Identificator
+#   LOG_JOBCLASS   Job class (SCRIPT,DAEMON,ORJI,HCRON,INTERACTIVE)
+#   LOG_CYCLOPE    Use cyclope (TRUE,FALSE)
 #
 # Functions:
 #   Log_Start     $Job $Version [$Paramfile]
@@ -35,106 +36,78 @@
 
 #----- Check for environment settings
 LOG_LEVEL=${LOG_LEVEL:=INFO}
-LOG_MODE=${LOG_MODE:=SCRIPT}
-LOG_MAIL=${LOG_MAIL:=""}
-LOG_MAILTITLE=${LOG_MAILTITLE:="Job Info"}
-LOG_JOBID=${LOG_JOBID:=""}
-LOG_FILE=${LOG_FILE:=""}
 LOG_TIME=${LOG_TIME:=0}
+LOG_PROC=${LOG_PROC:=0}
+LOG_FILE=${LOG_FILE:=""}
+
+LOG_MAILTO=${LOG_MAILTO:=""}
+LOG_MAILTITLE=${LOG_MAILTITLE:="Job Info"}
+
+LOG_JOB=${LOG_JOB:="Unknown"}
+LOG_JOBVERSION=${LOG_JOBVERSION:="Unknown"}
+LOG_JOBID=${LOG_JOBID:=""}
+LOG_JOBDATE=""
+LOG_JOBPATH=${LOG_JOBPATH:=""}
+LOG_JOBCLASS=${LOG_JOBCLASS:=SCRIPT}
+LOG_JOBCOMMAND=$0
+LOG_JOBARGS=$*
+
 LOG_OC=""
+LOG_CYCLOPE=FALSE
+LOG_CYCLOPEPATH=/home/binops/afse/eer/projets/Cyclope/jobs
 
 #----- Logger internal variables
-LogSecTime=`date +%s`
-LogSecStart=${SECONDS}
-LogSecEnd=${SECONDS}
-LogJob="Unknown"
-LogVersion="Unknown"
+LOG_SECTIME=`date +%s`
+LOG_SECSTART=`date +%s`
+LOG_SECEND=`date +%s`
+LOG_WARNINGS=0
 
-LogMUST=-1
-LogERROR=0
-LogWARNING=1
-LogINFO=2
-LogDEBUG=3
-LogLevelNo=${LogINFO}
+LOG_LEVELMUST=-1
+LOG_LEVELERROR=0
+LOG_LEVELWARNING=1
+LOG_LEVELINFO=2
+LOG_LEVELDEBUG=3
+LOG_LEVELNO=${LOG_LEVELINFO}
 
-function realpath {
-   fname=${1%/} # strips trailing '/'
-   while [ -L "$fname" ]; do
-      oldfname="$fname"
-      fname="$(command ls -l $fname)"
-      fname="${fname#*\> }"
-      if [ "$fname" = . ] ; then
-         fname="$(dirname $oldfname)"
-      elif echo $fname | grep -vq '^/' - ; then
-         fname="$(dirname $oldfname)/$fname"
-      fi
-   done
-   pushd $(dirname $fname) > /dev/null
-   fname=$(pwd -P)/$(basename $fname)
-   popd > /dev/null
-   echo $fname
-}
-
-function Log_MailIf {
-
-   subject=${1}
-   file=${2}
-
-   if [[ ${LOG_MODE} = "ALL" ]]; then
-      Log_Mail "$subject" $file
-   fi
-}
-
-function Log_Mail {
-
-   subject=${1}
-   file=${2}
-
-   if [[ ${LOG_MAIL} = "" ]] ; then
-      return 0
-   fi
-
-   if [[ -r ${file} ]] ;then
-      if [[ ${EER_ARCH} = IRIX64 ]] ; then
-         mailx -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${LOG_MAIL} < ${file}
-      else
-         mail -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${LOG_MAIL} < ${file}
-      fi
-   else
-      if [[ ${EER_ARCH} = IRIX64 ]] ; then
-         printf $file | mailx -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${LOG_MAIL}
-      else
-         printf $file | mail -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${LOG_MAIL}
-      fi
-   fi
-}
+#----------------------------------------------------------------------------
+# Nom      : <Log_Start>
+# Creation : Octobre 2009 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Afficher une message de demarrage standard.
+#
+# Parametres  :
+#    <Job>    : Nom de la job
+#    <Version>: Version de la job
+#    <Input>  : fichier d'entre (afin de recupere le temps d'attente en queue)
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
 
 function Log_Start {
 
-   LogSecStart=${SECONDS}
-   LogJob=${1}
-   LogVersion=${2}
+   LOG_SECSTART=`date +%s`
+   LOG_JOB=${1}
+   LOG_JOBVERSION=${2}
    in=${3}
 
    #----- Simulation run time ID.
-   if [[ ${LOG_JOBID} = "" ]] ; then
-      #----- Define run time ID if not defined.
-      LOG_JOBID=`date +%Y%m%d%H%M%S`
-   fi
+   LOG_JOBID="${LOG_JOBID}-`date +%Y%m%d_%H%M%S`"
 
    if [[ ! ${LOG_FILE} = "" ]] ; then
       rm -f ${LOG_FILE}
    fi
 
    Log_Print MUST "-------------------------------------------------------------------------------"
-   Log_Print MUST "Script              : ${LogJob}"
-   Log_Print MUST "Version             : ${LogVersion}"
+   Log_Print MUST "Script              : ${LOG_JOB}"
+   Log_Print MUST "Version             : ${LOG_JOBVERSION}"
    Log_Print MUST "Hostname            : `hostname`"
    Log_Print MUST "Architecture        : `uname -s`"
    Log_Print MUST "Run ID              : ${LOG_JOBID}"
 
-   if [[ ${LOG_MAIL} != "" ]] ; then
-      Log_Print MUST "E-mail Address      : ${LOG_MAIL}"
+   if [[ ${LOG_MAILTO} != "" ]] ; then
+      Log_Print MUST "E-mail Address      : ${LOG_MAILTO}"
    fi
 
    #----- Queue stuff
@@ -162,35 +135,80 @@ function Log_Start {
    Log_Print MUST "Start time          : `date +\"%c %Z\"`"
    Log_Print MUST "-------------------------------------------------------------------------------\n"
 
-   Log_MailIf "Job started" ${LOG_FILE}
+   if [[ ${LOG_JOBCLASS} = "INTERACTIVE" ]]; then
+      Log_Mail "Job started" ${LOG_FILE}
+   fi
+
+   #----- Activate Cylope links
+   Log_CyclopeStart
+
 }
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_End>
+# Creation : Octobre 2009 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Afficher une message de fin standard.
+#
+# Parametres  :
+#    <Status> : Code de retour de la job (0=ok, sinon erreur)
+#    <Exit>   : Sortie du prorgamme (Default=True)
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
 
 function Log_End {
 
    status=${1}
    exitnow=${2}
-   LogSecEnd=${SECONDS}
+
+   LOG_SECEND=`date +%s`
 
    Log_Print MUST "\n-------------------------------------------------------------------------------"
    if [[ ${status} -eq 0 ]]; then
-      Log_Print MUST "Status              : Job has terminated successfully."
+      Log_Print MUST "Status              : Job has terminated successfully (${LOG_WARNINGS} Warning(s))."
    else
       Log_Print MUST "Status              : Job has encountered some errors."
    fi
    Log_Print MUST "End time            : `date +\"%c %Z\"`"
-   Log_Print MUST "Total running time  : $(Log_Time `expr ${LogSecEnd} - ${LogSecStart}`)"
+   Log_Print MUST "Total running time  : $(Log_Time `expr ${LOG_SECEND} - ${LOG_SECSTART}`)"
    Log_Print MUST "-------------------------------------------------------------------------------\n"
 
    if [[ ${status} -eq 0 ]]; then
-      Log_MailIf "Job finished (NORMAL)" ${LOG_FILE}
+      if [[ ${LOG_JOBCLASS} = "INTERACTIVE" ]]; then
+         Log_Mail "Job finished (NORMAL)" ${LOG_FILE}
+      fi
    else
       Log_Mail "Job finished (ERROR)" ${LOG_FILE}
    fi
+
+   #----- Activate Cylope links
+   Log_CyclopeEnd $status
+   Log_CyclopeSysInfo
+   Log_CyclopeProcInfo
 
    if [[ ${exitnow} = "" ]]; then
       exit $status
    fi
 }
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_Print>
+# Creation : Octobre 2009 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Afficher une message standard.
+#
+# Parametres  :
+#    <Type>   : Type de mesage (MUST,ERROR,WARNING,INFO,DEBUG)
+#    <Message>: Message a afficher
+#    <Time>   : Temps specifique
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
 
 function Log_Print {
 
@@ -199,8 +217,13 @@ function Log_Print {
    time=${3}
 
    #----- Levels are MUST,ERROR,WARNING,INFO,DEBUG
-   eval lvl=\$\{Log$level\}
-   eval LogLevelNo=\$\{Log${LOG_LEVEL}\}
+   eval lvl=\$\{LOG_LEVEL$level\}
+   eval LOG_LEVELNO=\$\{LOG_LEVEL${LOG_LEVEL}\}
+
+   #----- If it is a  warning, add to count for end result
+   if [[ ${level} = "WARNING" ]]; then
+      LOG_WARNINGS=$((LOG_WARNINGS+1))
+   fi
 
    #----- Format the time if it is specified
    if [[ ${time} != "" ]]; then
@@ -213,7 +236,7 @@ function Log_Print {
       datetime="($(date)) "
    fi
 
-   if [[ ${lvl} -le ${LogLevelNo} ]]; then
+   if [[ ${lvl} -le ${LOG_LEVELNO} ]]; then
       if [[ ${level} = "MUST" ]] ; then
          datetime=""
          levels=""
@@ -229,11 +252,71 @@ function Log_Print {
       if [[ $level = "ERROR" ]] ; then
          printf "${datetime}${levels}${msg} ${time}\n" 1>&2
          if [[ ${LOG_OC} != "" ]]; then
-            oclog ${LogJob} x "${LOG_OC}\n\n${datetime}${levels}${msg} ${time}"
+            oclog ${LOG_JOB} x "${LOG_OC}\n\n${datetime}${levels}${msg} ${time}"
          fi
       fi
    fi
 }
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_Mail>
+# Creation : Octobre 2009 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Envoyer un message par courriel.
+#
+# Parametres  :
+#    <Subject>: Sujet du message
+#    <File>   : Fichier a envoyer
+#    <Address>: Adresse destinataire
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
+
+function Log_Mail {
+
+   subject=${1}
+   file=${2}
+   address=${3}
+
+   if [[ $address = "" ]]; then
+      address=${LOG_MAILTO}
+   fi
+
+   if [[ ${address} = "" ]] ; then
+      return 0
+   fi
+
+   if [[ -r ${file} ]] ;then
+      if [[ ${EER_ARCH} = IRIX64 ]] ; then
+         mailx -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${address} < ${file}
+      else
+         mail -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${address} < ${file}
+      fi
+   else
+      if [[ ${EER_ARCH} = IRIX64 ]] ; then
+         printf "$file" | mailx -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${address}
+      else
+         printf "$file" | mail -s "${LOG_MAILTITLE}: ${subject} (${LOG_JOBID})" ${address}
+     fi
+   fi
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_Time>
+# Creation : Mai 2010 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Formater un temps en secondes.
+#
+# Parametres  :
+#   <Sec>     : Secondes systeme
+#
+# Retour      :
+#   <Date>    : Date formatee
+#
+# Remarques   :
+#----------------------------------------------------------------------------
 
 function Log_Time {
 
@@ -245,4 +328,93 @@ function Log_Time {
    secs=$((secs % 60))
 
    printf "%02d:%02d:%02d\n" $hours $mins $secs
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_CyclopeStart>
+# Creation : Mai 2010 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Initialiser les informations du process pour cyclope.
+#
+# Parametres  :
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
+
+function Log_CyclopeStart {
+
+   if [[ ${LOG_CYCLOPE} = "TRUE" ]]; then
+      path=${LOG_CYCLOPEPATH}/${LOG_JOBID}
+
+      #----- Setup process info
+      mkdir $path
+      printf "Class     : ${LOG_JOBCLASS}\nJob       : ${LOG_JOB} ${LOG_JOBVERSION}\n" >$path/info.txt
+
+      if [[ ! ${SelfJobResubmit} = "" ]]; then
+         printf "Command   : $SelfJobResubmit\n" >>$path/info.txt
+         printf "Kill      : $SelfJobKill\n" >>$path/info.txt
+      else
+         printf "Command   : ssh `hostname` $LOG_JOBCOMMAND $LOG_JOBARGS $*\n" >>$path/info.txt
+         printf "Kill      : ssh `hostname` kill $$\n" >>$path/info.txt
+      fi
+      printf "Path      : ${LOG_JOBPATH}\nLog       : ${LOG_FILE}\nHostname  : `hostname`\nArch      : `uname -s`\nStart time: ${LOG_SECSTART}\n" >>$path/info.txt
+
+      chmod 666 $path/info.txt
+   fi
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Log_CyclopeEnd>
+# Creation : Mai 2010 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Finaliser les informations du process pour cyclope.
+#
+# Parametres  :
+#    <Status> : Code de retour de la job (0=ok, sinon erreur)
+#
+# Retour:
+#
+# Remarques :
+#----------------------------------------------------------------------------
+
+function Log_CyclopeEnd {
+
+   status=$1
+
+   if [[ ${LOG_CYCLOPE} = "TRUE" ]]; then
+      path=${LOG_CYCLOPEPATH}/${LOG_JOBID}
+
+      #----- Close process info
+      printf "End time  : ${LOG_SECEND}\nRun time  : $((LOG_SECEND-LOG_SECSTART))\n" >> $path/info.txt
+
+      if [[ ! $status -eq 0 ]]; then
+         printf "Status    : Error ($Status)\n" >> $path/info.txt
+      else
+         if [[ ${LOG_WARNINGS} -gt 0 ]]; then
+            printf "Status    : Warning (${LOG_WARNINGS})\n" >> $path/info.txt
+         else
+            printf "Status    : Success\n" >> $path/info.txt
+         fi
+      fi
+   fi
+}
+
+function Log_CyclopeSysInfo {
+
+   if [[ ${LOG_CYCLOPE} = "TRUE" ]]; then
+      path=${LOG_CYCLOPEPATH}/${LOG_JOBID}
+
+      printf "None\n" >> $path/sysinfo.txt
+   fi
+}
+
+function Log_CyclopeProcInfo {
+
+   if [[ ${LOG_CYCLOPE} = "TRUE" ]]; then
+      path=${LOG_CYCLOPEPATH}/${LOG_JOBID}
+
+      printf "None\n" >> $path/procinfo.txt
+   fi
 }
