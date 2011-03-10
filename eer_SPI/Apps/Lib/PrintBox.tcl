ee94848b1f920de8df1821ba4ebe63f3e189b9a7
Serge Trudel <Serge.Trudel@canada.ca>
2011-03-10 16:57:21 +0000

    Replace host dorval-ib by (TransmitHost).

--- eer_SPI/Apps/Lib/PrintBox.tcl
+++ eer_SPI/Apps/Lib/PrintBox.tcl
@@ -692,10 +692,10 @@ proc PrintBox::Print { Frame X Y Width Height { Format "" } } {
 
          if { $Print(WEBSite)=="WEATHEROFFICE_VAAC" } {
             set prefix [clock format [clock seconds] -format "%Y%m%d-%H%MZ" -gmt True]
-            set ErrCatch [catch  { exec $GDefs(Dir)/Script/CMOI_webprods.ksh $Param(FullName).$Print(Device) eer/data/vaac/current/${prefix}_[file tail $Param(FullName)].$Print(Device) $GDefs(TransmitUser) dorval-ib } MsgCatch ]
+            set ErrCatch [catch  { exec $GDefs(Dir)/Script/CMOI_webprods.ksh $Param(FullName).$Print(Device) eer/data/vaac/current/${prefix}_[file tail $Param(FullName)].$Print(Device) $GDefs(TransmitUser) $GDefs(TransmitHost) } MsgCatch ]
 
             if { $ErrCatch != 0 } {
-               Log::Print ERROR "Unable to transfert the $Param(FullName).$Print(Device) on weatheroffice.\n\n$MsgCatch"
+               Log::Print ERROR "Unable to transfert the $Param(FullName).$Print(Device) on weatheroffice via $GDefs(TransmitUser)@$GDefs(TransmitHost).\n\n$MsgCatch"
             }
 
          } else {
