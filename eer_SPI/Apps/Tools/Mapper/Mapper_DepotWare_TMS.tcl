e655a02877d3ac30ff267fd283066c1a4c83e051
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2012-01-20 20:08:28 +0000

    Added a bunch of TMS servers.

--- eer_SPI/Apps/Tools/Mapper/Mapper_DepotWare_TMS.tcl
+++ eer_SPI/Apps/Tools/Mapper/Mapper_DepotWare_TMS.tcl
@@ -18,7 +18,20 @@
 namespace eval Mapper::DepotWare::TMS {
    variable Param
 
-   set Param(Depots) { { OpenStreetMap TMS OpenStreetMap } { MapQuest TMS MapQuest} { BlueMarble TMS BlueMarble } { MetaCarta TMS MetaCarta } }
+   set Param(Depots) {
+      { OpenStreetMap TMS OpenStreetMap }
+      { MapQuest TMS MapQuest}
+      { BlueMarble TMS BlueMarble }
+      { MetaCarta TMS MetaCarta }
+      { OneMoon TMS OneMoon }
+      { OneEarth TMS OneEarth }
+      { MSVirtualEarth TMS MSVirtualEarth }
+      { Google-Maps TMS Google-Maps }
+      { Google-Satellite TMS Google-Satellite }
+      { Google-SatelliteHybrid TMS Google-SatelliteHybrid }
+      { Google-Terrain TMS Google-Terrain }
+      { Google-TerrainHybrid TMS Google-TerrainHybrid }
+      { "ESRI Server" TMS "ESRIServer" }}
 }
 
 #-------------------------------------------------------------------------------
@@ -78,27 +91,27 @@ proc  Mapper::DepotWare::TMS::Select { Tree Branch Path URL } {
    set file $Mapper::DepotWare::Data(CachePath)/[string map { / "" ? "" " " "" : "" } $Layer].xml
 
    set Param(OpenStreetMap) "<GDAL_WMS>
-    <Service name=\"TMS\">
-        <ServerUrl>http://tile.openstreetmap.org/\${z}/\${x}/\${y}.png</ServerUrl>
-    </Service>
-    <DataWindow>
-        <UpperLeftX>-20037508.34</UpperLeftX>
-        <UpperLeftY>20037508.34</UpperLeftY>
-        <LowerRightX>20037508.34</LowerRightX>
-        <LowerRightY>-20037508.34</LowerRightY>
-        <TileLevel>19</TileLevel>
-        <TileCountX>1</TileCountX>
-        <TileCountY>1</TileCountY>
-        <YOrigin>top</YOrigin>
-    </DataWindow>
-    <Projection>EPSG:900913</Projection>
-    <BlockSizeX>256</BlockSizeX>
-    <BlockSizeY>256</BlockSizeY>
-    <BandsCount>3</BandsCount>
-    <Cache>
-       <Path>[file rootname $file]</Path>
-       <Depth>2</Depth>
-    </Cache>
+   <Service name=\"TMS\">
+       <ServerUrl>http://tile.openstreetmap.org/\${z}/\${x}/\${y}.png</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>19</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
 </GDAL_WMS>"
 
    set Param(MapQuest) "<GDAL_WMS>
@@ -126,55 +139,263 @@ proc  Mapper::DepotWare::TMS::Select { Tree Branch Path URL } {
 </GDAL_WMS>"
 
 
-   set Param(BlueMarble) "<GDAL_WMS>
-    <Service name=\"TileService\">
-        <Version>1</Version>
-        <ServerUrl>http://s0.tileservice.worldwindcentral.com/getTile?</ServerUrl>
-        <Dataset>bmng.topo.bathy.200401</Dataset>
-    </Service>
-    <DataWindow>
-        <UpperLeftX>-180.0</UpperLeftX>
-        <UpperLeftY>90.0</UpperLeftY>
-        <LowerRightX>180.0</LowerRightX>
-        <LowerRightY>-90.0</LowerRightY>
-   <SizeX>65536</SizeX>
-   <SizeY>32768</SizeY>
-   <TileLevel>7</TileLevel>
-    </DataWindow>
-    <Projection>EPSG:4326</Projection>
-    <BlockSizeX>512</BlockSizeX>
-    <BlockSizeY>512</BlockSizeY>
-    <BandsCount>3</BandsCount>
+set Param(BlueMarble) "<GDAL_WMS>
+   <Service name=\"TMS\">
+      <ServerUrl>http://s3.amazonaws.com/com.modestmaps.bluemarble/\$\{z\}-r\$\{y\}-c\$\{x\}.jpg</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>9</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(MetaCarta) "<GDAL_WMS>
+   <Service name=\"TMS\">
+      <ServerUrl>http://labs.metacarta.com/wms-c/Basic.py</ServerUrl>
+      <Layer>basic</Layer>
+      <Format>png</Format>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-180.0</UpperLeftX>
+      <UpperLeftY>90.0</UpperLeftY>
+      <LowerRightX>180.0</LowerRightX>
+      <LowerRightY>-90.0</LowerRightY>
+      <TileLevel>19</TileLevel>
+   <TileCountX>2</TileCountX>
+   <TileCountY>1</TileCountY>
+   </DataWindow>
+   <Projection>EPSG:4326</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(OneMoon) "<GDAL_WMS>
+   <Service name=\"TiledWMS\">
+   <ServerUrl>http://onmoon.jpl.nasa.gov/wms.cgi?</ServerUrl>
+   <TiledGroupName>Clementine</TiledGroupName>
+   </Service>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(OneEarth) "<GDAL_WMS>
+   <Service name=\"TiledWMS\">
+      <ServerUrl>http://onearth.jpl.nasa.gov/wms.cgi?</ServerUrl>
+      <TiledGroupName>Daily Planet</TiledGroupName>
+      <Change key=\"\$\{time\}\">2009-10-12</Change>
+   </Service>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(OneEarthSRTM) "<GDAL_WMS>
+   <Service name=\"TiledWMS\">
+      <ServerUrl>http://onearth.jpl.nasa.gov/wms.cgi?</ServerUrl>
+      <TiledGroupName>Global SRTM Elevation</TiledGroupName>
+   </Service>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(MSVirtualEarth) "<GDAL_WMS>
+   <Service name=\"VirtualEarth\">
+      <ServerUrl>http://a\$\{server_num\}.ortho.tiles.virtualearth.net/tiles/a\$\{quadkey\}.jpeg?g=90</ServerUrl>
+   </Service>
+   <MaxConnections>4</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(Google-Terrain) "<GDAL_WMS>
+   <!--
+   Data is subject to term of use detailed at http://code.google.com/intl/nl/apis/maps/terms.html and http://www.google.com/intl/en_ALL/help/terms_maps.html
+   -->
+   <Service name=\"TMS\">
+      <ServerUrl>http://mt.google.com/vt/lyrs=t&amp;x=\$\{x\}&amp;y=\$\{y\}&amp;z=\$\{z\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>20</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>5</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(Google-TerrainHybrid) "<GDAL_WMS>
+   <!--
+   Data is subject to term of use detailed at http://code.google.com/intl/nl/apis/maps/terms.html and http://www.google.com/intl/en_ALL/help/terms_maps.html
+   -->
+   <Service name=\"TMS\">
+      <ServerUrl>http://mt.google.com/vt/lyrs=p&amp;x=\$\{x\}&amp;y=\$\{y\}&amp;z=\$\{z\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>20</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>5</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(Google-Maps) "<GDAL_WMS>
+   <!--
+   Data is subject to term of use detailed at http://code.google.com/intl/nl/apis/maps/terms.html and http://www.google.com/intl/en_ALL/help/terms_maps.html
+   -->
+   <Service name=\"TMS\">
+      <ServerUrl>http://mt.google.com/vt/lyrs=m&amp;x=\$\{x\}&amp;y=\$\{y\}&amp;z=\$\{z\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>20</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>5</MaxConnections>
     <Cache>
        <Path>[file rootname $file]</Path>
        <Depth>2</Depth>
     </Cache>
 </GDAL_WMS>"
 
+set Param(Google-Satellite) "<GDAL_WMS>
+   <!--
+   Data is subject to term of use detailed at http://code.google.com/intl/nl/apis/maps/terms.html and http://www.google.com/intl/en_ALL/help/terms_maps.html
+   -->
+   <Service name=\"TMS\">
+      <ServerUrl>http://mt.google.com/vt/lyrs=s&amp;x=\$\{x\}&amp;y=\$\{y\}&amp;z=\$\{z\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>20</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>5</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
 
-set Param(MetaCarta) "<GDAL_WMS>
-    <Service name=\"TMS\">
-        <ServerUrl>http://labs.metacarta.com/wms-c/Basic.py</ServerUrl>
-        <Layer>basic</Layer>
-        <Format>png</Format>
-    </Service>
-    <DataWindow>
-        <UpperLeftX>-180.0</UpperLeftX>
-        <UpperLeftY>90.0</UpperLeftY>
-        <LowerRightX>180.0</LowerRightX>
-        <LowerRightY>-90.0</LowerRightY>
-        <TileLevel>19</TileLevel>
-   <TileCountX>2</TileCountX>
-   <TileCountY>1</TileCountY>
-    </DataWindow>
-    <Projection>EPSG:4326</Projection>
-    <BlockSizeX>256</BlockSizeX>
-    <BlockSizeY>256</BlockSizeY>
-    <BandsCount>3</BandsCount>
-    <Cache>
-       <Path>[file rootname $file]</Path>
-       <Depth>2</Depth>
-    </Cache>
+set Param(Google-SatelliteHybrid) "<GDAL_WMS>
+   <!--
+   Data is subject to term of use detailed at http://code.google.com/intl/nl/apis/maps/terms.html and http://www.google.com/intl/en_ALL/help/terms_maps.html
+   -->
+   <Service name=\"TMS\">
+      <ServerUrl>http://mt.google.com/vt/lyrs=y&amp;x=\$\{x\}&amp;y=\$\{y\}&amp;z=\$\{z\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>20</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>5</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
+</GDAL_WMS>"
+
+set Param(ESRIServer) "<GDAL_WMS>
+   <Service name=\"TMS\">
+   <ServerUrl>http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/\$\{z\}/\$\{y\}/\$\{x\}</ServerUrl>
+   </Service>
+   <DataWindow>
+      <UpperLeftX>-20037508.34</UpperLeftX>
+      <UpperLeftY>20037508.34</UpperLeftY>
+      <LowerRightX>20037508.34</LowerRightX>
+      <LowerRightY>-20037508.34</LowerRightY>
+      <TileLevel>17</TileLevel>
+      <TileCountX>1</TileCountX>
+      <TileCountY>1</TileCountY>
+      <YOrigin>top</YOrigin>
+   </DataWindow>
+   <Projection>EPSG:900913</Projection>
+   <BlockSizeX>256</BlockSizeX>
+   <BlockSizeY>256</BlockSizeY>
+   <BandsCount>3</BandsCount>
+   <MaxConnections>10</MaxConnections>
+   <Cache>
+      <Path>[file rootname $file]</Path>
+      <Depth>2</Depth>
+   </Cache>
 </GDAL_WMS>"
 
    set f [open $file w]
