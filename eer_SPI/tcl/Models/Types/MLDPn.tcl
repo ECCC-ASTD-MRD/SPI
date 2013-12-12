dec7eab2693252fbef559eb7bf937662a927e47b
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2013-12-12 14:48:17 +0000

    Do not generate RSMC metfields on backward simulation.

--- eer_SPI/tcl/Models/Types/MLDPn.tcl
+++ eer_SPI/tcl/Models/Types/MLDPn.tcl
@@ -87,7 +87,7 @@ proc MLDPn::Launch { } {
    MLDPn::CreateScriptInput
 
    #----- Launch meteorological fields script for RSMC response.
-   if { $Sim(SrcType) == "ACCIDENT" && [file exists $Sim(Path)/tmp/data_std_pres.in] } {
+   if { $Sim(SrcType) == "ACCIDENT" && !$Sim(Backward) && [file exists $Sim(Path)/tmp/data_std_pres.in] } {
       Log::Print INFO "Launching RSMC meteorological fields script on local host ($GDefs(Host))."
       set metdate [clock format $Sim(Sim0Secs) -format "%Y%m%d%H" -gmt True]
       set ErrorCode [catch { exec $env(EER_DIRSCRIPT)/GenerateMetfields.tcl $Sim(Path)/tmp $metdate $metdate $Sim(Path)/tmp/data_std_pres.in >& $Sim(Path)/tmp/GenerateMetfields.out & } Message]
