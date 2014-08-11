37990688164de19cddeb8cb80289bf4c84fa0e93
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2014-08-11 15:14:46 +0000

    Finalized Style system for RPN data.

--- eer_SPI/tcl/Lib/Styles.tcl
+++ eer_SPI/tcl/Lib/Styles.tcl
@@ -12,6 +12,12 @@
 #    Gestion des styles
 #
 # Fonctions:
+#   Styles::Read   { }
+#   Styles::Save   { Frame { Style "" } }
+#   Styles::Delete { Frame Style }
+#   Styles::Apply  { Style }
+#   Styles::Widget { Frame { Update False } }
+#   Styles::Write  { Channel Spec { Style "" } }
 #
 # Remarques :
 #
@@ -24,30 +30,35 @@ catch { SPI::Splash "Loading Package Styles 1.0" }
 namespace eval Styles { } {
    variable Lbl
    variable Msg
+   variable Bubble
    variable Data
    
-   set Data(Name)  ""
-   set Data(Names) {}
+   set Data(Style)  ""
+   set Data(Styles) {}
    
    #----- Definitions des labels
    set Lbl(Yes)       { "Oui" "Yes" }
    set Lbl(No)        { "Non" "No" }
+   set Lbl(Save)      { "Sauvegarder le style courant" "Save current style" }
+   set Lbl(Del)       { "Supprimer le style courant"   "Delete current style" }
 
    #----- Definitions des textes
    set Msg(Del)   { "Voulez-vous vraiment supprimer ce style ?"
                     "Do you really want to delete this style ?" }
 
-   set Msg(Exist) { "Ce style déja, voulez-vous l'écraser ?"
-                    "This style already exists do you wish to overwrite it ?" }
-   set Msg(Save)  { "Sauvegarder le style courant" "Save the current style" }
-                    
+   set Msg(Exist)    { "Ce style déja, voulez-vous l'écraser ?" "This style already exists do you wish to overwrite it ?" }
+   set Msg(Save)     { "Sauvegarder le style courant"           "Save the current style" }
+   set Msg(Name)     { "Nom du style:"                           "Style name: " }
+   set Msg(NotValid) { "Ce style n'est pas valide"              "This is not a valid style" }
+   
+   set Bubble(Style) { "Styles définissant les paramêtres de rendue des données" "Styles defining the rendering parameters of the data" }
 }
 
 #----------------------------------------------------------------------------
 # Nom      : <Styles::Read>
 # Creation : Juillet 2014 - J.P. Gauthier - CMC/CMOE
 #
-# But      : Lit le fichier de definitions de styles
+# But      : Read style definitions
 #
 # Parametres :
 #
@@ -69,10 +80,171 @@ proc Styles::Read { } {
    }
    
    foreach path $paths {
-      if { [file exists $path/Style] } {
-         source $env(HOME)/.spi/Style
+      foreach file [glob -nocomplain $path/Style/*] {
+         source $file
+         lappend Data(Styles) [file rootname [file tail $file]]
       }
    }
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Styles::Save>
+# Creation : Aout 2014 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Save a style
+#
+# Parametres :
+#   <Frame>  : Widget's frame
+#   <Style>  : Style name
+#
+# Retour:
+#
+# Remarques :
+#
+#----------------------------------------------------------------------------
+
+proc Styles::Save { Frame { Style "" } } {
+   global env
+   variable Data
+   variable Lbl
+   variable Msg
+
+   if { $Style!="" } { 
+      
+      #----- Check if valid
+      if { ![dataspec is $FSTD::Param(Spec)] } {
+         Dialog::Error . $Msg(NotValid)
+         return
+      }
+      
+      #----- Clear bad char
+      set Style [string map { " " _ } $Style]
+      
+      #----- Check if it exists
+      if { [file exist $env(HOME)/.spi/Style/$Style.tcl] } {
+         if { [set nok [Dialog::Default . 200 WARNING $Msg(Exist) "" 1 $Lbl(Yes) $Lbl(No)]] } {
+            return
+         }
+      }
+      
+      #----- Save the style
+      set f [open $env(HOME)/.spi/Style/$Style.tcl w]
+      
+      Styles::Write $f $FSTD::Param(Spec) $Style
+      
+      close $f
+      
+      #----- Reload the styles
+      Styles::Read
+      Styles::Widget $Frame True
+   }
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Styles::Delete>
+# Creation : Aout 2014 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Delete a style
+#
+# Parametres :
+#   <Frame>  : Widget's frame
+#   <Style>  : Style name
+#
+# Retour:
+#
+# Remarques :
+#
+#----------------------------------------------------------------------------
+
+proc Styles::Delete { Frame Style } {
+   global env
+   variable Data
+   variable Lbl
+   variable Msg
+   
+   if { [Dialog::Default . 200 WARNING $Msg(Del) "\n\n\t$Style" 0 $Lbl(No) $Lbl(Yes)] } {
+      if { [set idx [lsearch -exact $Data(Styles) $Style]]!=-1 } {
+         set Data(Styles) [lreplace $Data(Styles) $idx $idx]
+      }
+      
+      Styles::Widget $Frame True
+      
+      file delete $env(HOME)/.spi/Style/$Style.tcl
+   }
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Styles::Apply>
+# Creation : Aout 2014 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Apply style
+#
+# Parametres :
+#   <Style>  : Style name
+#
+# Retour:
+#
+# Remarques :
+#
+#----------------------------------------------------------------------------
+
+proc Styles::Apply { Style } {
+   variable Data
+   
+   set Data(Style) $Style
+   
+   FSTD::ParamGet $Style
+   FSTD::ParamPut 
+   FSTD::ParamSet
+
+   SPI::ParamsApply
+}
+
+#----------------------------------------------------------------------------
+# Nom      : <Styles::Widget>
+# Creation : Aout 2014 - J.P. Gauthier - CMC/CMOE
+#
+# But      : Build style interface wigdet
+#
+# Parametres :
+#   <Frame>  : Frame into which to build the widget
+#   <Update> : Simply update style list
+#
+# Retour:
+#
+# Remarques :
+#
+#----------------------------------------------------------------------------
+
+proc Styles::Widget { Frame { Update False } } {
+   global GDefs
+   variable Data
+   variable Lbl
+   variable Bubble
+
+   if { $Update } {
+      $Frame.styles.menu delete 3 end
+   } else {
+      menubutton $Frame.styles -image PALETTE -menu $Frame.styles.menu -bd 1 -relief groove -bd 2 
+      menu $Frame.styles.menu -tearoff 0 -bd 1
+      $Frame.styles.menu add command -label [lindex $Lbl(Save) $GDefs(Lang)] -command "Styles::Save $Frame \[Dialog::Get . \$Styles::Msg(Save) \$Styles::Msg(Name)\]"
+      $Frame.styles.menu add command -label [lindex $Lbl(Del) $GDefs(Lang)]  -command "Styles::Delete $Frame \$Styles::Data(Style)"
+      $Frame.styles.menu add separator
+   }
+   
+   Bubble::Create $Frame.styles $Bubble(Style)
+   
+   #----- Insert styles in menu
+   set n 0
+   foreach style $Data(Styles) {
+      $Frame.styles.menu add command -columnbreak [expr $n>19] -label $style -command "Styles::Apply $style"  
+      
+      if { [incr n]>20 } {
+         set n 0
+      }
+   }
+
+   return $Frame.styles
 }
 
 #----------------------------------------------------------------------------
@@ -82,6 +254,9 @@ proc Styles::Read { } {
 # But      : Ecrit un style dans un fichier
 #
 # Parametres :
+#   <Channel>: Channel channel to write to
+#   <Spec>   : Dataspec definition
+#   <Style>  : Style name, otherwise, use Spec name
 #
 # Retour:
 #
@@ -89,20 +264,24 @@ proc Styles::Read { } {
 #
 #----------------------------------------------------------------------------
 
-proc Styles::Write { File Spec } {
-
-   puts $File "\n#----- Style $Spec"
+proc Styles::Write { Channel Spec { Style "" } } {
    
    set fonts {}
    set cmaps {}
 
+   if { $Style=="" } {
+      set Style $Spec
+   }
+   
+   puts $Channel "\n#----- Style $Style"
+   
    set cmap [dataspec configure $Spec -colormap]
-   if { [colormap is $cmap] && [lsearch -exact $cmaps $cmap]==-1 } {
-      lappend cmaps $cmap
-      puts $File "\ncatch { colormap create $cmap }"
-      puts $File "colormap control $cmap -del"
-      puts $File "colormap control $cmap -list { [colormap control $cmap -list] }"
-      puts $File "colormap configure $cmap  -RGBAratio [colormap configure $cmap -RGBAratio] -MMratio [colormap configure $cmap -MMratio] \
+   if { [colormap is $cmap] && [lsearch -exact $cmaps CMAP$Style]==-1 } {
+      lappend cmaps CMAP$Style
+      puts $Channel "\ncatch { colormap create CMAP$Style }"
+      puts $Channel "colormap control CMAP$Style -del"
+      puts $Channel "colormap control CMAP$Style -list { [colormap control $cmap -list] }"
+      puts $Channel "colormap configure CMAP$Style  -RGBAratio [colormap configure $cmap -RGBAratio] -MMratio [colormap configure $cmap -MMratio] \
          -curve red [colormap configure $cmap -curve red] -curve green [colormap configure $cmap -curve green] -curve blue [colormap configure $cmap -curve blue] -curve alpha [colormap configure $cmap -curve alpha] \
          -invertx red [colormap configure $cmap -invertx red] -invertx green [colormap configure $cmap -invertx green] -invertx blue [colormap configure $cmap -invertx blue] -invertx alpha [colormap configure $cmap -invertx alpha] \
          -inverty red [colormap configure $cmap -inverty red] -inverty green [colormap configure $cmap -inverty green] -inverty blue [colormap configure $cmap -inverty blue] -inverty alpha [colormap configure $cmap -inverty alpha] \
@@ -112,14 +291,14 @@ proc Styles::Write { File Spec } {
    }
 
    set font [dataspec configure $Spec -font]
-   if { $font!="" && [lsearch -exact $fonts $font]==-1 } {
+   if { $font!="" && [lsearch -exact $fonts FONT$Style]==-1 } {
       lappend fonts $font
-      puts $File "\ncatch { font create $font }"
-      puts $File "font configure $font -family [font configure $font -family] -weight [font configure $font -weight] -size [font configure $font -size]\
+      puts $Channel "\ncatch { font create FONT$Style }"
+      puts $Channel "font configure FONT$Style -family [font configure $font -family] -weight [font configure $font -weight] -size [font configure $font -size]\
             -slant [font configure $font -slant] -underline [font configure $font -underline] -overstrike [font configure $font -overstrike]"
    }
 
-   puts $File "\ndataspec create \"$Spec\" -factor [dataspec configure $Spec -factor] -value [dataspec configure $Spec -value]\
+   puts $Channel "\ndataspec create \"$Style\" -factor [dataspec configure $Spec -factor] -value [dataspec configure $Spec -value]\
       -size [dataspec configure $Spec -size] -width [dataspec configure $Spec -width] -unit \"[dataspec configure $Spec -unit]\"\
       -desc \"[dataspec configure $Spec -desc]\" -icon \"[dataspec configure $Spec -icon]\" -mark \"[dataspec configure $Spec -mark]\"\
       -color \"[dataspec configure $Spec -color]\" -fill \"[dataspec configure $Spec -fill]\" -activefill \"[dataspec configure $Spec -activefill]\"\
@@ -131,7 +310,7 @@ proc Styles::Write { File Spec } {
       -rendergrid [dataspec configure $Spec -rendergrid] -min \"[dataspec configure $Spec -min]\" -max \"[dataspec configure $Spec -max]\"\
       -intervals \"[dataspec configure $Spec -intervals]\" -interlabels \"[dataspec configure $Spec -interlabels]\" -intervalmode [dataspec configure $Spec -intervalmode] \
       -interpdegree \"[dataspec configure $Spec -interpdegree]\" -extrapdegree \"[dataspec configure $Spec -extrapdegree]\" \
-      -sample \"[dataspec configure $Spec -sample]\" -stipple \"[dataspec configure $Spec -stipple]\" -colormap \"[dataspec configure $Spec -colormap]\" -font \"[dataspec configure $Spec -font]\" \
+      -sample \"[dataspec configure $Spec -sample]\" -stipple \"[dataspec configure $Spec -stipple]\" -colormap \"CMAP$Style\" -font \"FONT$Style\" \
       -texsample \"[dataspec configure $Spec -texsample]\" -texsize \"[dataspec configure $Spec -texsize]\" -texres \"[dataspec configure $Spec -texres]\" \
       -interpolation \"[dataspec configure $Spec -interpolation]\" -topography \"[dataspec configure $Spec -topography]\" -topographyfactor \"[dataspec configure $Spec -topographyfactor]\" \
       -mask \"[dataspec configure $Spec -mask]\" -light \"[dataspec configure $Spec -light]\" -labelvar \"[dataspec configure $Spec -labelvar]\" \
@@ -139,58 +318,4 @@ proc Styles::Write { File Spec } {
       -extrude \"[dataspec configure $Spec -extrude]\" -extrudefactor \"[dataspec configure $Spec -extrudefactor]\""
 }
 
-proc Styles::Save { { Style "" } } {
-   global env
-   variable Data
-   variable Lbl
-   variable Msg
-
-   #----- Check if it exists
-   if { $Style!="" && [set idx [lsearch -exact $Data(Names) $Style]]!=-1 } {
-      if { [set nok [Dialog::Default . 200 WARNING $Msg(Exist) "" 1 $Lbl(Yes) $Lbl(No)]] } {
-         return
-      }
-   }
-   
-   #----- Backup previous
-   if { [file exists $env(HOME)/.spi/Style] } {
-      file rename -force $env(HOME)/.spi/Style $env(HOME)/.spi/Style.old
-   }
-   
-   #----- Save all styles
-   set f [open $env(HOME)/.spi/Style w]
-   
-   foreach name $Data(Names) {
-      Styles::Write $f $name
-   }
-   
-   close $f
-}
-
-proc Styles::Delete { Style } {
-   variable Data
-   variable Lbl
-   variable Msg
-   
-   if { [Dialog::Default . 200 WARNING $Msg(Del) "\n\n\t$Data(Name)" 0 $Lbl(No) $Lbl(Yes)] } {
-      if { [set idx [lsearch -exact $Data(Names) $Style]]!=-1 } {
-         set Data(Names) [lreplace $Data(Names) $idx $idx]
-      }
-      
-      Styles::Save
-   }
-}
-
-proc Styles::Widget { Frame Command } {
-
-   frame $Frame.styles
-      ComboBox::Create $Frame.styles.name Styles::Data(Name) edit sorted nodouble -1 $Styles::Data(Names) 5 6 $Command
-      button $Frame.styles.save   -image PALETTESAVE  -relief flat -bd 1 -overrelief raised \
-         -command "Styles::Save \[Dialog::Get . \$Styles::Msg(Save) \$Styles::Data(Name)\]"
-      button $Frame.styles.del    -image PALETTEDEL   -relief flat -bd 1 -overrelief raised \
-         -command "Styles::Delete $Frame.cam.sel"
-      pack $Frame.styles.name -side left -fill x -expand True -padx 2
-      pack $Frame.styles.save $Frame.styles.del -side left -padx 2
-   
-   return $Frame.styles
-}
+Styles::Read
