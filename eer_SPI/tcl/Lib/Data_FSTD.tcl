acfa88903a2edd20578ea0664513f0ed5aedf678
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2016-06-28 17:13:57 +0000

    Fixed combination or range and intervals reverting to NaN after an option select on FSTD configure.

--- eer_SPI/tcl/Lib/Data_FSTD.tcl
+++ eer_SPI/tcl/Lib/Data_FSTD.tcl
@@ -905,8 +905,8 @@ proc FSTD::ParamSet { { Spec "" } } {
 
    set inter $Param(Intervals)
    set label {}
-   set min   ""
-   set max   ""
+   set min   [dataspec configure $Spec -min]
+   set max   [dataspec configure $Spec -max]
 
    #----- Verifier pour un range plutot que des niveaux
 
@@ -958,6 +958,7 @@ proc FSTD::ParamSet { { Spec "" } } {
    if  { $Param(IntervalMode)=="INTERVAL" || $Param(IntervalMode)=="LINEAR" || $Param(IntervalMode)=="LOGARITHMIC" || $Param(IntervalMode)=="RSMC" } {
       dataspec configure $Spec -intervalmode $Param(IntervalMode) $Param(IntervalParam)
       set Param(Intervals) [dataspec configure $Spec -intervals]
+   puts stderr .$Param(Intervals).$min.$max
    } else {
       dataspec configure $Spec -intervals $inter -interlabels $label -intervalmode $Param(IntervalMode) $Param(IntervalParam)
    }
