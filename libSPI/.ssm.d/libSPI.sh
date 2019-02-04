78bbcb7d8f42bf380f06b3a0920cc95d91160c25
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2019-02-04 22:13:58 +0000

    Tweaks for external build.

--- libSPI/.ssm.d/libSPI.sh
+++
@@ -1,8 +0,0 @@
-#----- Profile for external users (non SSM)
-
-#----- GDAL data
-export GDAL_PAM_ENABLED=NO
-export CPL_LOG=/dev/null
-
-#----- Force BURP table dir since it's hardcoded with RMN and problem outside cmc
-export MA_TABLEBURP_PERSONNELLE=${SPI_LIB}/share/rmn/table_b_bufr
