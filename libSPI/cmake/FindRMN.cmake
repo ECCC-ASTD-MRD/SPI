c6b32e609d4a8a33e4a0acb0c214498bd9367576
Eric Legault-Ouellet <eric.legault-ouellet@canada.ca>
2020-12-04 14:01:47 -0500

    IMported target is now marked as static when the static component is loaded

--- libSPI/cmake/FindRMN.cmake
+++ libSPI/cmake/FindRMN.cmake
@@ -14,19 +14,21 @@ find_path(RMN_INCLUDE_DIR
 
 # [[DOC]] for find_library https://cmake.org/cmake/help/latest/command/find_library.html
 if("shared" IN_LIST RMN_FIND_COMPONENTS)
-    if("threaded" IN_LIST RMN_FIND_COMPONENTS)
-        find_library(RMN_LIBRARY
-           NAMES rmnsharedPTHRD
-           PATHS ${EC_LD_LIBRARY_PATH})
-    else()
-       find_library(RMN_LIBRARY
-           NAMES rmnshared
-           PATHS ${EC_LD_LIBRARY_PATH})
-    endif()
+   set(type SHARED)
+   if("threaded" IN_LIST RMN_FIND_COMPONENTS)
+      find_library(RMN_LIBRARY
+         NAMES rmnsharedPTHRD
+         PATHS ${EC_LD_LIBRARY_PATH})
+   else()
+      find_library(RMN_LIBRARY
+         NAMES rmnshared
+         PATHS ${EC_LD_LIBRARY_PATH})
+   endif()
 else()
-    find_library(RMN_LIBRARY
-        NAMES rmn
-        PATHS ${EC_LD_LIBRARY_PATH})
+   set(type STATIC)
+   find_library(RMN_LIBRARY
+      NAMES rmn
+      PATHS ${EC_LD_LIBRARY_PATH})
 endif()
 
 string(REGEX MATCH ".*/libs/([0-9]+\\.[0-9]+)(\\.[0-9])?(-[a,b][0-9]*)?.*/" null ${RMN_LIBRARY})
@@ -47,7 +49,7 @@ if(RMN_FOUND)
 
    mark_as_advanced(RMN_INCLUDE_DIR RMN_LIBRARY)
 
-   add_library(RMN::RMN SHARED IMPORTED)
+   add_library(RMN::RMN ${type} IMPORTED)
    set_target_properties(RMN::RMN PROPERTIES
       IMPORTED_LOCATION             ${RMN_LIBRARY}
       INTERFACE_INCLUDE_DIRECTORIES ${RMN_INCLUDE_DIR}
@@ -56,12 +58,12 @@ if(RMN_FOUND)
 endif()
 
 if("rpnpy" IN_LIST RMN_FIND_COMPONENTS)
-    message("Component rpnpy requested")
-    # Let's say we don't find it
-    set(RMN_rpnpy_FOUND)
-    if(${RMN_FIND_REQUIRED_rpnpy})
-        message(FATAL_ERROR "Could not find required component rpnpy")
-    else()
-        message("Could not find component rpnpy")
-    endif()
+   message("Component rpnpy requested")
+   # Let's say we don't find it
+   set(RMN_rpnpy_FOUND)
+   if(${RMN_FIND_REQUIRED_rpnpy})
+      message(FATAL_ERROR "Could not find required component rpnpy")
+   else()
+      message("Could not find component rpnpy")
+   endif()
 endif()
